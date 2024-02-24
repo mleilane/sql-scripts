1fcac9499395289f12c8b0c12fcb4fc72111e16e
Maria Leilane <81912351+mleilane@users.noreply.github.com>
2024-02-24 18:16:15 -0300

    Add files via upload
    Resumo sobre o código:
    
    Objetivo: Criar uma tabela para armazenar informações sobre produtos e  dados do cliente. Criamos a coluna, em seguida foi atribuído o seu tipo e o tamanho.
    
    A palavra-chave CHAR define uma coluna de caracteres fixos. Isso significa que cada valor na coluna terá o mesmo tamanho, mesmo que alguns valores sejam mais curtos que o tamanho especificado. Os espaços em branco serão adicionados ao final de valores mais curtos para completar o tamanho especificado.
    A palavra-chave VARCHAR define uma coluna de caracteres variáveis. Isso significa que cada valor na coluna pode ter um tamanho diferente, até o tamanho máximo especificado.
    O número após CHAR ou VARCHAR indica o tamanho máximo da coluna em caracteres.
    O tipo SMALLMONEY é usado para armazenar valores monetários com precisão de 4 casas decimais.

---
+++ CRIAR_TABELA - CLIENTE.sql
@@ -0,0 +1,38 @@
+/*CRIANDO O BANCO DE DADOS */
+CREATE DATABASE SUCOS_VENDAS
+
+/*CRIANDO A TABELA CLIENTES */
+CREATE TABLE [TABELA DE CLIENTES]( 
+[CPF] [CHAR] (11),
+[NOME] [VARCHAR]  (150),
+[RUA] [VARCHAR]  (150),
+[COMPLEMENTO] [VARCHAR]  (150),
+[BAIRRO] [VARCHAR]  (150),
+[ESTADO] [CHAR]  (2),
+[CEP] [CHAR]  (8),
+[DATA DE NASCIMENTO] [DATE],
+[IDADE] [SMALLINT],
+[SEXO] [CHAR]  (1),
+[LIMITE DE CREDITO] [MONEY],
+[VOLUME MINIMO] [FLOAT],
+[PRIMEIRA COMPRA] [BIT]
+);
+
+
+INSERT INTO [TABELA DE CLIENTES]
+VALUES
+('1471156710','Erica Carvalho','R. Iriquitia',' ','Jardins','SP','80012212','1990-09-01',32,'F',1700000000,24500,0),
+('19290992743','Fernando Cavalcante','R. Dois de Fevereiro',' ','Agua Santa','RJ','22000000','2000-02-12',23,'M',1000000000,20000,1),
+('2600586709','Cesar Teixeira','Rua Conde de Bonfim',' ','Tijuca','RJ','22020001','2000-03-12',23,'M',1200000000,22000,0),
+('3623344710','Marcos Nogueira','Av. Pastor Martin Luther King Junior',' ','Inhauma','RJ','22002012','1995-01-13',28,'M',1100000000,22000,1),
+('492472718','Eduardo Jorge','R. Volta Grande',' ','Tijuca','RJ','22012002','1994-07-19',28,'M',750000000,9500,1),
+('50534475787','Abel Silva','Rua Humaita',' ','Humaita','RJ','22000212','1995-09-11',27,'M',1700000000,26000,0),
+('5576228758','Petra Oliveira','R. Benicio de Abreu',' ','Lapa','SP','88192029','1995-11-14',27,'F',700000000,16000,1),
+('5648641702','Paulo Cesar Mattos','Rua Helio Beltrao',' ','Tijuca','RJ','21002020','1991-08-30',31,'M',1200000000,22000,0),
+('5840119709','Gabriel Araujo','R. Manuel de Oliveira',' ','Santo Amaro','SP','80010221','1985-03-16',37,'M',1400000000,21000,1),
+('7771579779','Marcelo Mattos','R. Eduardo Luís Lopes',' ','Bras','SP','88202912','1992-03-25',30,'M',1200000000,20000,1),
+('8502682733','Valdeci da Silva','R. Srg. Edison de Oliveira',' ','Jardins','SP','82122020','1995-10-07',27,'M',1100000000,19000,0),
+('8719655770','Carlos Eduardo','Av. Gen. Guedes da Fontoura',' ','Jardins','SP','81192002','1983-12-20',39,'M',2000000000,24000,0),
+('9283760794','Edson Meilelles','R. Pinto de Azevedo',' ','Cidade Nova','RJ','22002002','1995-10-07',27,'M',1500000000,25000,1),
+('94387575700','Walber Lontra','R. Cel. Almeida',' ','Piedade','RJ','22000201','1989-06-20',33,'M',600000000,12000,1),
+('95939180787','Fabio Carvalho','R. dos Jacarandas da Peninsula',' ','Barra da Tijuca','RJ','22002020','1992-01-05',31,'M',900000000,18000,1);
